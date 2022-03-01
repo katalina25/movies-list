570da565a5c5804a41da9b91b3ac35aee82f36fd
katalina25 <katalina.guerrero02@inacapmail.cl>
2022-03-01 16:55:38 -0300

    Animation

--- src/Movie.js
+++ src/Movie.js
@@ -3,7 +3,7 @@ import { motion } from "framer-motion";
 function Movie({ movie }) {
     return (
 
-        <motion.div animate={{ opacity: 1 }} initial={{ opacity: 0 }} exit={{ opacity: 0 }} layout>
+        <motion.div animate={{ opacity: 1, scale: 1 }} initial={{ opacity: 0, scale: 0 }} exit={{ opacity: 0 }} layout>
 
             <h2>{movie.title}</h2>
             <img src={'https://image.tmdb.org/t/p/w500' + movie.backdrop_path} alt="" />
